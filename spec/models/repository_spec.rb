b12c889bcca2367d1f5a8c3e6878fd222da636de
Martin N <mr.avgp@googlemail.com>
2012-08-27 22:51:54 +0200

    Beefing up the repository_spec a little

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Repository do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid when no name is specified" do
+    Repository.new(:url => "abc").should_not be_valid
+  end
+  
+  it "is not valid when no url is specified" do
+    Repository.new(:name => "test").should_not be_valid
+  end
 end
