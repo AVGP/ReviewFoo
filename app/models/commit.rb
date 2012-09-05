b328bfb8f0349a230c957cbedf2fc6f62a00044f
Martin Naumann <martin.naumann@centralway.com>
2012-09-05 16:24:15 +0200

    Commit model now has accessible property branch_name

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,4 +1,4 @@
 class Commit < ActiveRecord::Base
   belongs_to :repository
-  attr_accessible :author, :date, :hash_id, :message, :repository_id
+  attr_accessible :author, :date, :hash_id, :message, :repository_id, :branch_name
 end
