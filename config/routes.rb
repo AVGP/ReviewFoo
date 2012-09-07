fab7676c854c7890bdde866e10c8ae2a31d8bd27
Martin Naumann <martin.naumann@centralway.com>
2012-09-07 21:51:31 +0200

    Removing public/index.html and set root route to repositories/

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ ReviewFoo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'repositories#index'
 
   # See how all your routes lay out with "rake routes"
 
