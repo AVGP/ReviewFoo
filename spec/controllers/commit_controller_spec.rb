a4c21fee2c7c61555c43bb78a0a08cc29e1c201a
Martin Naumann <martin.naumann@centralway.com>
2012-09-20 08:28:16 +0200

    Adjusting commit_controller spec to reflect changed route for commit#show

--- spec/controllers/commit_controller_spec.rb
+++ spec/controllers/commit_controller_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 describe CommitController do
 
-  describe "GET 'show'" do
+  describe "GET 'commit/:hash'" do
     it "returns http success" do
-      get 'show'
+      repo = Repository.create(:name => "Test 1", :url => "/some/path/A")
+      aCommit = Commit.create(
+      :repository_id => repo.id, 
+      :hash_id => "a123", 
+      :message => "Test 1", 
+      :branch_name => "default"
+      )    
+      aCommit = Commit.create()
+      puts commit_path(aCommit.hash_id.to_s)
       response.should be_success
     end
   end
