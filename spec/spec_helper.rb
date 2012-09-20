3db81b3d32739516c134446a72960e4695b68261
Martin Naumann <martin.naumann@centralway.com>
2012-09-20 08:03:53 +0200

    Using DatabaseCleaner to clean up the db for each spec do-block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,11 +32,11 @@
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.  
-#    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :truncation
 
     config.use_transactional_fixtures = false
-#    config.before(:each) { DatabaseCleaner.start }
-#    config.after(:each) { DatabaseCleaner.clean }
+    config.before(:each) { DatabaseCleaner.start }
+    config.after(:each) { DatabaseCleaner.clean }
   end
 
 #end
