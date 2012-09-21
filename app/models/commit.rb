9890266bd9421e1857ee793344b707fd72d4262d
Martin N <avgp_2k@yahoo.de>
2012-09-22 00:58:30 +0200

    Adding default value for Commit.accepted

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,6 +2,12 @@ class Commit < ActiveRecord::Base
   belongs_to :repository
   has_many :commit_diffs
   attr_accessible :author, :date, :hash_id, :message, :repository_id, :branch_name, :accepted
+
+  after_initialize :defaults
+  
+  def defaults
+    self.accepted = 0 unless self.accepted
+  end
   
   def to_param
     self.hash_id
