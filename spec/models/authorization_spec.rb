7949415a4c2ff712c70b3002e3b5b3039ac9da59
Martin N <avgp_2k@yahoo.de>
2012-09-23 16:51:26 +0200

    Implementing the authorization specs

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Authorization do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be valid if provider or uid is missing" do
+    Authorization.new(:provider => "github").should_not be_valid
+    Authorization.new(:uid => 123).should_not be_valid
+  end
+  
+  it "should be valid, if provider and uid are provided" do
+    Authorization.new(:provider => "github", :uid => 123).should be_valid
+  end
 end
