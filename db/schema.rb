80b9969e5245096703e137503aec21cd83a4b372
Martin N <avgp_2k@yahoo.de>
2012-09-23 16:50:44 +0200

    Migrating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120923104134) do
+ActiveRecord::Schema.define(:version => 20120923112729) do
+
+  create_table "authorizations", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "commit_diffs", :force => true do |t|
     t.integer  "commit_id"
