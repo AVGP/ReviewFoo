8b4eb723932accf72d7d51747a22130484afcec8
Martin N <avgp_2k@yahoo.de>
2012-09-23 13:23:37 +0200

    Migrated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120914052933) do
+ActiveRecord::Schema.define(:version => 20120923104134) do
 
   create_table "commit_diffs", :force => true do |t|
     t.integer  "commit_id"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(:version => 20120914052933) do
   create_table "repositories", :force => true do |t|
     t.string   "name"
     t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
