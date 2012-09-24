0313b59ec2c6a73ccfc68d80e36561b2217701c0
Martin Naumann <martin.naumann@centralway.com>
2012-09-24 20:29:54 +0200

    Removed bogus example from sessions_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe SessionsController do
+  
+  before :each do
+    OmniAuth.config.test_mode = true
 
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      :provider => 'github',
+      :uid => '123456'
+    })
+  end
+  
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get '/auth/github/callback'
       response.should be_success
     end
   end
