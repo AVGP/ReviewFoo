0b016844d8c1106f65ccb34623ce9c5e54d2bdcc
Martin N <avgp_2k@yahoo.de>
2012-10-01 08:27:30 +0200

    Adding login vs. signup logic to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,25 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    @user = User.new(
-      :name => auth_hash["info"]["nickname"],
-      :email => auth_hash["info"]["email"]
-    )
+    @auth = Authorization.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
+    if @auth.nil?
+      @user = User.new(
+        :name => auth_hash["info"]["nickname"],
+        :email => auth_hash["info"]["email"]
+      )
     
-    @user.authorizations.build(
-      :provider => auth_hash["provider"], 
-      :uid => auth_hash["uid"]
-    )
-    @user.save
+      @user.authorizations.build(
+        :provider => auth_hash["provider"], 
+        :uid => auth_hash["uid"]
+      )
+      @user.save
 
-    render :text => "Welcome #{@user.name}"
+      session[:user_id] = 1
+      redirect_to "/"
+    else
+      session[:user_id] = 1
+      redirect_to "/"
+    end
   end
 
   def failure
